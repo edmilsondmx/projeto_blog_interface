0ff4a9c38c08fa387f095d6ceaf3bc69162ba80f
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-22 13:46:38 -0300

    Create PostUnit Component

---
+++ src/components/atoms/PostUnit.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import PostUnit from "./PostUnit";
+
+export default {
+  title: "Components/Atoms/PostUnit",
+  component: PostUnit,
+};
+
+export const usage = () => (
+  <PostUnit
+    title="sunt aut facere repellat"
+    description="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    user="Edmilson Gomes"
+  />
+);
