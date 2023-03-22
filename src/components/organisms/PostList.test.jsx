c4346e36850819ab371d55ab15866442f2cc4e17
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-22 13:46:52 -0300

    Create PostList component

---
+++ src/components/organisms/PostList.test.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, screen } from "../../test-utils";
+import PostList from "./PostList";
+import PostUnit from "../atoms/PostUnit";
+
+it("renders PostList with children", () => {
+  render(
+    <PostList>
+      <h1>Título</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </PostList>
+  );
+
+  expect(screen.getByText(/Lorem ipsum/i)).toBeInTheDocument();
+});
+
+it("renders PostList with Postunit", () => {
+  render(
+    <PostList>
+      <PostUnit
+        title="sunt aut facere repellat"
+        description="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        user="Edmilson Gomes"
+      />
+    </PostList>
+  );
+
+  expect(
+    screen.getByText(/sunt aut facere repellat/i, { selector: "h3" })
+  ).toBeInTheDocument();
+});
