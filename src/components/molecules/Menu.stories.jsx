45571b25a53742636355d77a55ac3191edbc2779
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-22 11:36:19 -0300

    Create menu component

---
+++ src/components/molecules/Menu.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Menu from "./Menu";
+
+export default {
+  title: "Components/Molecules/Menu",
+  component: Menu,
+};
+
+export const usage = () => (
+  <Menu
+    image="https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+    alt="avatar para perfil"
+    user="Edmilson Gomes"
+  />
+);
