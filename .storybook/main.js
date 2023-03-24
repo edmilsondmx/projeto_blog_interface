8286c2448678211c414ebe903cbc1369d1ea202f
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-24 17:20:19 -0300

    Add addon view port

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,15 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/styles/**/*.stories.@(js|jsx|mdx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-viewport",
     "@storybook/addon-knobs",
   ],
   framework: "@storybook/react",
