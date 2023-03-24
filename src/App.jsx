0fc30e2884d7c6565a809ae270f15130d31346d9
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-24 17:21:22 -0300

    Add button mode theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useState } from "react";
 
 import GlobalStyle from "./styles/GlobalStyle";
 import ThemeProvider from "./styles/ThemeProvider";
 import Routes from "./routes";
+import styled from "styled-components";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+const IconContainer = styled.p`
+  font-size: 2rem;
+  padding: 2px;
+  position: fixed;
+  right: 5px;
+  top: 40px;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.main};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
+        <IconContainer onClick={toggleTheme}>
+          {theme === "dark" ? <FaSun /> : <FaMoon />}
+        </IconContainer>
         <Routes />
       </Router>
     </ThemeProvider>
