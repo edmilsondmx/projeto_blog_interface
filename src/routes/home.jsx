0e6827efacaabab3cb5bd323942c14023a545fc2
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-25 11:44:11 -0300

    Add call in API

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
 import HomePage from "../components/pages/Home";
+import blogFetchApi from "../axios/config";
 
-const Home = () => <HomePage />;
+const Home = () => {
+  const [post, setPost] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetchApi.get("/posts");
+
+      const data = response.data;
+
+      setPost(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return <HomePage postList={post} />;
+};
 
 export default Home;
--- src/routes/postDetail.jsx
+++ src/routes/postDetail.jsx
@@ -1,6 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import PostDetailPage from "components/pages/PostDetail";
+import blogFetchApi from "../axios/config";
 
-const PostDetail = () => <PostDetailPage />;
+const PostDetail = () => {
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+
+  const { id } = useParams();
+
+  const getPost = async () => {
+    try {
+      const response = await blogFetchApi.get(`/posts/${id}`);
+
+      const data = response.data;
+
+      setPost(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getComments = async () => {
+    try {
+      const response = await blogFetchApi.get(`/post/${id}/comments`);
+
+      const data = response.data;
+
+      setComments(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+    getComments();
+  }, []);
+
+  return <PostDetailPage post={post} commentsList={comments} />;
+};
 
 export default PostDetail;
