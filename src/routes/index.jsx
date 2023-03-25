3fd2c92a602134a43c7936fbefd100225faeb19b
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-25 11:41:50 -0300

    add route user data

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,12 +5,14 @@ import Home from "../routes/home";
 import About from "../routes/about";
 import PostDetail from "./postDetail";
 import Error404 from "./error";
+import UserData from "./userData";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/sobre" element={<About />} />
-    <Route path="/comentarios" element={<PostDetail />} />
+    <Route path="/post/:id/comentarios" element={<PostDetail />} />
+    <Route path="/user/:userId/perfil" element={<UserData />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
 );
