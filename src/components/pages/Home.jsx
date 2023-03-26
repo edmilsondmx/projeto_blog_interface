949f7e2e7e530842503fdcbffa5fc73e827a3d93
Edmilson Gomes <edmilsondmx@gmail.com>
2023-03-26 15:38:36 -0300

    Add pagination in home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { useState } from "react";
 
 import Menu from "../molecules/Menu";
 import Section from "../atoms/Section";
@@ -8,6 +9,7 @@ import PostList from "../organisms/PostList";
 import PostUnit from "../atoms/PostUnit";
 import Footer from "../organisms/Footer";
 import PostType from "models/types/PostType";
+import Pagination from "components/molecules/Pagination";
 
 const TitleDiv = styled.div`
   text-align: center;
@@ -18,31 +20,55 @@ const TitleDiv = styled.div`
   }
 `;
 
-const Home = ({ postList }) => (
-  <>
-    <Menu />
-    <Section>
-      <TitleDiv>
-        <h2>Últimos Posts</h2>
-      </TitleDiv>
-      <PostList>
-        {postList.length === 0 ? (
-          <h6>Carregando...</h6>
-        ) : (
-          postList.map((post) => (
-            <PostUnit
-              key={post.id}
-              title={post.title}
-              description={post.body}
-              to={`/post/${post.id}/comentarios`}
-            />
-          ))
-        )}
-      </PostList>
-    </Section>
-    <Footer />
-  </>
-);
+const Loading = styled.h3`
+  color: ${(props) => props.theme.colors.main};
+`;
+
+const Home = ({ postList }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const postPerPage = 10;
+
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = postList?.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <>
+      <Menu />
+      <Section>
+        <TitleDiv>
+          <h2>Últimos Posts</h2>
+        </TitleDiv>
+        <PostList>
+          {currentPosts.length === 0 ? (
+            <Loading>Carregando...</Loading>
+          ) : (
+            currentPosts.map((post) => (
+              <PostUnit
+                key={post.id}
+                title={post.title}
+                description={post.body}
+                to={`/post/${post.id}/comentarios`}
+              />
+            ))
+          )}
+        </PostList>
+        <Pagination
+          postsPerPage={postPerPage}
+          totalPosts={postList?.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </Section>
+      <Footer />
+    </>
+  );
+};
 
 Home.defaultProps = {
   postList: [],
