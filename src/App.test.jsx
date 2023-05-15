989e717e67808ab21b4b94ee5ed0d0cea99403d2
Edmilson Gomes <edmilsondmx@gmail.com>
2023-05-15 16:32:38 -0300

    Update setTheme app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,8 +3,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 it("renders call to action", () => {
+  window.scrollTo = jest.fn();
   render(<App />);
 
-  const Element = screen.getByText(/Blog/i);
+  const Element = screen.getByText(/Home/i, { selector: "a" });
   expect(Element).toBeInTheDocument();
 });
